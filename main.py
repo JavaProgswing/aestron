a1b3c49dca7fa0cfccc458eec9147c35122b7625
JavaProgswing <yashasviallen@gmail.com>
2023-05-06 12:05:25 +0530

    TESTING!

--- main.py
+++ main.py
@@ -6290,11 +6290,6 @@ class Captcha(commands.Cog):
         usage="#channelone #channeltwo ...",
     )
     @commands.guild_only()
-    @commands.check_any(is_bot_staff())
-    async def parsechecker(self, ctx, *, args):
-        await ctx.send(args)
-
-    @commands.guild_only()
     @commands.check_any(is_bot_staff(), commands.has_permissions(manage_channels=True))
     async def verifyreadadd(self, ctx, *, list_textstagevoicechannels):
         print("VERIFY READ ADD")
